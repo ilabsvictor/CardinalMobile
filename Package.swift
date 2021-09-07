25094313651f03ed51c8f4d725a69e2a75892c3c
Victor Zhu <ghoyelo@gmail.com>
2021-09-07 14:00:29 +0800

    Support spm

---
+++ Package.swift
@@ -0,0 +1,22 @@
+
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "CardinalMobile",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "CardinalMobile",
+            targets: ["CardinalMobile"])
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "CardinalMobile",
+            path: "CardinalMobile.framework")
+    ]
+)
